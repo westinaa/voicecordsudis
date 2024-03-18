86f6c0eb6374957c5d8868141f024faa77607299
westinaa <162748407+westinaa@users.noreply.github.com>
2024-03-18 03:29:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+import discord
 import os
-import sys
-import json
-import time
-import requests
-import websocket
+from discord.ext import commands
 from keep_alive import keep_alive
+
+client=commands.Bot(command_prefix=".", self_bot=True, help_command=None)
 
 GUILD_ID = os.getenv("guild_id")
 CHANNEL_ID = os.getenv("channel_id")
@@ -16,31 +15,18 @@ if not usertoken:
     print("[ERROR] Please add a token inside Secrets.")
     sys.exit()
 
-headers = {"Authorization": usertoken, "Content-Type": "application/json"}
-
-validate = requests.get('https://canary.discordapp.com/api/v9/users/@me', headers=headers)
-if validate.status_code != 200:
-  print("[ERROR] Your token might be invalid. Please check it again.")
-  sys.exit()
-
-userinfo = requests.get('https://canary.discordapp.com/api/v9/users/@me', headers=headers).json()
-username = userinfo["username"]
-discriminator = userinfo["discriminator"]
-userid = userinfo["id"]
-
-def joiner(token):
-    start = json.loads(ws.recv())
-    heartbeat = start['d']['heartbeat_interval']
-    auth = {"op": 2,"d": {"token": token,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"s": None,"t": None}
-    vc = {"op": 4,"d": {"guild_id": GUILD_ID,"channel_id": CHANNEL_ID,"self_mute": SELF_MUTE,"self_deaf": SELF_DEAF}}
-    time.sleep(heartbeat / 1000)
-
-def run_joiner():
-  os.system("clear")
-  print(f"Logged in as {username}#{discriminator} ({userid}).")
-  while True:
-    joiner(usertoken, status)
-    time.sleep(30)
+@client.event
+async def on_ready():
+    os.system("clear")
+    print(f'Logged in as {client.user} ({client user.id)')
+    vc = discord.utils.get(client.get_guild(GUILD_ID).channels, id = CHANNEL_ID
+    await vs.guild.change_voice_state(channel=vc, self_mute=True, self_deaf=True)
+    print(f"Succesfully joined {vc.name} ({vc.id})")
 
 keep_alive()
-run_joiner()
+
+
+
+
+
+
